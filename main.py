c85240b97ac4a1f759f4290ae2eed45ca96defef
Clement <39395559+clmenet0504@users.noreply.github.com>
2022-09-30 10:01:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 
 
 intents = discord.Intents.default()
-intents.message_content = True
+# intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 
